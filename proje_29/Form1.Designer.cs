08b9d3da63ee72b88bb78cace8996c323b63f413
henesbzdmr <concinik42@gmail.com>
2024-04-26 18:02:32 +0300

    first commit

---
+++ proje_29/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace proje_29
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(203, 108);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 19);
+            label1.TabIndex = 0;
+            label1.Text = "sayı :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(370, 101);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 19);
+            label2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(498, 101);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 33);
+            button1.TabIndex = 2;
+            button1.Text = "sayı üret";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(203, 153);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 36);
+            button2.TabIndex = 3;
+            button2.Text = "tek sayı";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(370, 153);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 36);
+            button3.TabIndex = 4;
+            button3.Text = "çift sayı";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(203, 207);
+            label3.Name = "label3";
+            label3.Size = new Size(47, 19);
+            label3.TabIndex = 5;
+            label3.Text = "puan :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(370, 207);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 19);
+            label4.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ proje_29/Form1.cs
@@ -0,0 +1,80 @@
+namespace proje_29
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int puan = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rastgele = new Random(); // kütüphane
+            int random = rastgele.Next(0, 100);
+            label1.Text = random.ToString();
+            button2.Enabled = true;
+            button3.Enabled = true;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int sayý = Convert.ToInt32(label1.Text);
+            int bölüm = sayý / 2;
+            int kalan = sayý - (bölüm * 2);
+            if (kalan == 1)
+            {
+                puan = puan + 10;
+
+            }
+            if (kalan == 0)
+            {
+                puan = puan - 10;
+
+            }
+            label4.Text = puan.ToString();
+            button2.Enabled = false; // buton aktiflik
+            button3.Enabled = false;
+           
+            if (puan == 100)
+            {
+                MessageBox.Show("kazandýnýz");
+                Application.Exit(); // uygulamadan çýkma
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int sayý = Convert.ToInt32(label1.Text);
+            int bölüm = sayý / 2;
+            int kalan = sayý - (bölüm * 2);
+            if (kalan == 0)
+            {
+                puan = puan + 10;
+
+            }
+            if (kalan == 1)
+            {
+                puan = puan - 10;
+
+            }
+            label4.Text = puan.ToString();
+            button2.Enabled = false;
+            button3.Enabled = false;
+
+
+            if (puan == 100)
+            {
+                MessageBox.Show("kazandýnýz");
+                Application.Exit();
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
